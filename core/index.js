cf85cda21716e21349fd6e3f6d72a30791e7341a
Zach Dahl <z.schtauffen@gmail.com>
2016-10-28 09:33:31 -0500

    update plugins logic

--- core/index.js
+++ core/index.js
@@ -7,23 +7,7 @@
    *
    *	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
    */
-  function Jackalope (options, plugins) {
-    var state = options.state || function (x) { return x }
-    var model = options.model
-    var actions = options.actions
-
-    var J = Object.create(Jackalope)
-
-    if (plugins) {
-      plugins(J, { state: state, model: model, actions: actions })
-    }
-
-    J.actions = J.actions || (actions ? J.bindActions(actions) : {})
-    J.present = J.present || model.present(state(J.actions))
-    J.present({ type: J.INIT, data: null })
-
-    return J
-  }
+  var slice = Array.prototype.slice
 
   Jackalope.version = '0.3.0'
   Jackalope.INIT = '@@jackalope/init'
@@ -32,12 +16,34 @@
     context = context || this
 
     if (typeof actions === 'function')
-      return function () { context.present(actions.apply(null, Array.prototype.slice.call(arguments))) }
+      return function () { context.present(actions.apply(null, slice.call(arguments))) }
 
     return Object.keys(actions).reduce(function (out, key) {
       out[key] = context.bindActions(actions[key], context)
       return out
     }, {})
+  }
+  
+  function Jackalope (options) {
+    var state = options.state || function (x) { return x }
+    var model = options.model
+    var actions = options.actions
+
+    var J = Object.create(Jackalope)
+
+    if (arguments.length > 1) {
+      slice.call(arguments, 1).reduce(function (out, plug) {
+        return out.concat(plug)
+      }, []).forEach(function (plugin) {
+        plugin(J, { state: state, model: model, actions: actions })
+      })
+    }
+
+    J.actions = J.actions || (actions ? J.bindActions(actions) : {})
+    J.present = J.present || model.present(state(J.actions))
+    J.present({ type: J.INIT, data: null })
+
+    return J
   }
 
   if (typeof module !== 'undefined') module['exports'] = Jackalope
