1bca24c6a1c815738a828824143b7c4a0d5aaa5e
Zach Dahl <zach.dahl@solutiondesign.com>
2016-10-28 09:49:47 -0500

    add start script, clean core

--- core/index.js
+++ core/index.js
@@ -1,4 +1,4 @@
-!function () {
+(function (slice) {
   /**
    *  Jackalope - a microscopic and modular SAM container
    *	Copyright (c) 2016, Zach Dahl <z.schtauffen@gmail.com>
@@ -7,8 +7,6 @@
    *
    *	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
    */
-  var slice = Array.prototype.slice
-
   Jackalope.version = '0.3.0'
   Jackalope.INIT = '@@jackalope/init'
 
@@ -23,7 +21,7 @@
       return out
     }, {})
   }
-  
+
   function Jackalope (options) {
     var state = options.state || function (x) { return x }
     var model = options.model
@@ -46,6 +44,6 @@
     return J
   }
 
-  if (typeof module !== 'undefined') module['exports'] = Jackalope
+  if (typeof module !== 'undefined') module.exports = Jackalope
   else window.Jackalope = Jackalope
-}();
+})(Array.prototype.slice);
